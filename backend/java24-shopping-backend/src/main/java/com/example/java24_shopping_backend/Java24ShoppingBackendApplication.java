734ae2b042e9d317e107e091151ec2421b1b4d67
Nguyễn Hữu Thành <121446657+huuthanh36@users.noreply.github.com>
2025-09-07 16:44:35 +0700

    Add backend

---
+++ backend/java24-shopping-backend/src/main/java/com/example/java24_shopping_backend/Java24ShoppingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.java24_shopping_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java24ShoppingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java24ShoppingBackendApplication.class, args);
+	}
+
+}
---
+++ backend/java24-shopping-backend/src/test/java/com/example/java24_shopping_backend/Java24ShoppingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java24_shopping_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java24ShoppingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
